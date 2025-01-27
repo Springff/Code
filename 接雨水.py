c2bf9c09d5d0686eb34414c9bf61d4318556bba6
Chuny <166700944+Springff@users.noreply.github.com>
2025-01-28 00:20:42 +0800

    Update 接雨水.py

--- 接雨水.py
+++ 接雨水.py
@@ -1,3 +1,24 @@
 # 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
 
  
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        stack = []
+        t = 0
+
+        for i,h in enumerate(height):
+            while stack and h > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                distance = i - stack[-1] - 1
+                high = min(h,height[stack[-1]]) - height[top]
+                t = t + high*distance
+            stack.append(i)
+        
+        return t
