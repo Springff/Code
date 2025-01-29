a9bb08c6dce7411490a8b20a01e8195def92b92b
Springff <cychai09@gmail.com>
2025-01-29 21:58:10 +0800

    Input.py

---
+++ Input.py
@@ -0,0 +1,44 @@
+# 单行输入
+import sys
+# 将字符串转换为列表
+a = sys.stdin.readline().strip().split(',')
+# 将列表中的字符串转换为整数
+a = list(map(int,a))
+
+print(a)
+print(type(a))
+
+
+
+# 多行输入
+data = []
+while True:
+    # 多行输入不要直接split，否则会报错，最后一行没有逗号
+    line = sys.stdin.readline().strip()
+    if line == '':
+        break
+    line = list(map(int,line.split(',')))
+    data.append(line)
+    
+# print(data)
+# print(type(data))
+# print(type(data[0]))
+
+
+
+
+# 简易输入
+
+a = input().split(',')
+a = list(map(int,a))
+
+data = []
+while True:
+    line = input()
+    if not line:
+        break
+    line = list(map(int,line.split(',')))
+    data.append(line)
+
+print(data)
+    
