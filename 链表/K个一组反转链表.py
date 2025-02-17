cfb5d01146df08bf80d419176cd6921e3207071a
CY <cychai09@gmail.com>
2025-02-18 00:04:50 +0800

    k个一组翻转链表

--- 链表/K个一组反转链表.py
+++ 链表/K个一组反转链表.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def reverseKGroup(self, head, k) :
+    def reverseKGroup1(self, head, k) :
         # 首先判断是否有k个节点可以交换
         # 采用头插法进行翻转
         # 预先建一个空节点就不用注意第一次是否需要翻转的问题了
@@ -41,6 +41,48 @@ class Solution:
             else:
                 return out.next
         return out.next
+    
+    def reverseKGroup(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        # 双指针，第一个指针先走k步，来判断是否翻转
+
+        out = ListNode()
+        out.next = head
+        fast,slow = out,out
+        while fast.next:
+            for _ in range(k):
+                if not fast.next: 
+                    return out.next
+                fast = fast.next
+            next = fast.next
+            pre,last = self.remove(slow.next,fast)
+            slow.next = pre
+            last.next = next
+            fast,slow = last,last
+        return out.next
+
+
+    
+
+    def remove(self,head1,head2):
+        # 输入第一个和最后一个节点，翻转后返回第一个和最后一个节点
+        out = ListNode()
+        last = head1
+        q = head1
+        prev = head2.next ###注意注意注意
+        while q!=prev:
+            p = q
+            q = q.next 
+            p.next = out.next
+            out.next = p
+        
+        return out.next,head1
+
+
 
 solution = Solution()
 list = [1,2,3,4,5]
