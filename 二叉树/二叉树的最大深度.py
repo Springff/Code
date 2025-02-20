ca492a94c2e6fb8bf387bba50014889ec1555f57
Chuny <166700944+Springff@users.noreply.github.com>
2025-02-20 16:26:12 +0800

    Update and rename 2.py to 二叉树的最大深度.py

---
+++ 二叉树/二叉树的最大深度.py
@@ -0,0 +1,29 @@
+
+# 给定一个二叉树 root ，返回其最大深度。
+
+# 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        # 遍历二叉树，纪录最大深度
+        
+        return self.Depth(root,0)
+
+    def Depth(self,root,depth):
+        if root == None:
+            return depth
+        depth += 1
+        left_depth = self.Depth(root.left,depth)
+        right_depth = self.Depth(root.right,depth)
+        return max(left_depth,right_depth)
+
