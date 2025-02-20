6d88716f0b20f12c4230dca279f5dda283c65282
Chuny <166700944+Springff@users.noreply.github.com>
2025-02-20 16:43:40 +0800

    Update 二叉树的最大深度.py

--- 二叉树/二叉树的最大深度.py
+++ 二叉树/二叉树的最大深度.py
@@ -26,4 +26,28 @@ class Solution(object):
         left_depth = self.Depth(root.left,depth)
         right_depth = self.Depth(root.right,depth)
         return max(left_depth,right_depth)
+    def diedai(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        # 中序遍历二叉树，纪录最大深度
+        depth = 0
+        max_d = 0
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                depth += 1
+                if depth > max_d:
+                    max_d = depth
+                stack.append((curr,depth))
+                curr = curr.left
+            curr,depth = stack.pop()
+            
+            
+            curr = curr.right
+               
+            
 
+        return max_d
