aad05643d88cb2e08591f915130a66fa9a6d4187
Springff <cychai09@gmail.com>
2025-02-25 13:20:01 +0800

    updata

--- 二叉树/有序数组转为二叉搜索树.py
+++ 二叉树/有序数组转为二叉搜索树.py
@@ -1,11 +1,11 @@
 # 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def sortedArrayToBST(self, nums):
         """
