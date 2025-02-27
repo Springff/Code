06d7e78e388f4262240f25330939965b80954e1a
Chuny <166700944+Springff@users.noreply.github.com>
2025-02-27 14:36:55 +0800

    Create 前序中序构造二叉树.py

---
+++ 二叉树/前序中序构造二叉树.py
@@ -0,0 +1,24 @@
+# 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        # 给定先序和中序可以确定根节点，递归建树
+
+        if not preorder:
+            return
+        root = TreeNode(preorder[0])
+        index = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:index+1],inorder[:index])
+        root.right = self.buildTree(preorder[index+1:],inorder[index+1:])
+        return root
