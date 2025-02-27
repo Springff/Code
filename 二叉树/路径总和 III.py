2e498af247449f87780fa92123ed1fc2fb160083
Chuny <166700944+Springff@users.noreply.github.com>
2025-02-27 23:49:42 +0800

    Update 路径总和 III.py

--- 二叉树/路径总和 III.py
+++ 二叉树/路径总和 III.py
@@ -31,3 +31,30 @@ class Solution(object):
         count += self.Count(root.left,targetSum-root.val)
         count += self.Count(root.right,targetSum-root.val)
         return count
+
+    def pathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: int
+        """
+        # 使用前缀和的思想，纪录当前节点所有前缀之和，判断字典中是否存在前缀和减去目标值的值存在
+        if not root:
+            return 0
+        prefix_sum = {}
+        prefix_sum[0] = 1
+        count = self.Counts(prefix_sum,0,root,targetSum)
+        return count
+
+    def Counts(self,prefix_sum,current,root,targetSum):
+        if not root:
+            return 0
+        target = current+root.val-targetSum
+        count = prefix_sum.get(target,0)
+        prefix_sum[current+root.val] = prefix_sum.get(current+root.val,0)+1
+        count += self.Counts(prefix_sum,current+root.val,root.left,targetSum)
+        count += self.Counts(prefix_sum,current+root.val,root.right,targetSum)
+
+        # 注意回溯
+        prefix_sum[current+root.val] -= 1
+        return count
