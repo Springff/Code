063654ba63c020f081f9c7854ad89b6d3dea1234
Chuny <166700944+Springff@users.noreply.github.com>
2025-03-12 11:12:09 +0800

    Update 买卖股票的最佳时机.py

--- 贪心算法/买卖股票的最佳时机.py
+++ 贪心算法/买卖股票的最佳时机.py
@@ -1,1 +1,22 @@
+# 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
 
+# 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+# 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        mini = prices[0]
+        for i in range(len(prices)):
+            if prices[i]-mini>profit:
+                profit = prices[i]-mini
+            if prices[i]<mini:
+                mini = prices[i]
+
+        return profit
