97438683325e73d50ff2413bed2221fe70a211b0
Chuny <166700944+Springff@users.noreply.github.com>
2025-03-16 09:38:29 +0800

    Update 只出现一次的数字.py

--- 技巧/只出现一次的数字.py
+++ 技巧/只出现一次的数字.py
@@ -1,3 +1,18 @@
 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
 
 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # a^0=a
+        # a^a=0
+        # a^a^b=b
+        if len(nums)==1:
+            return nums[0]
+        result = 0
+        for num in nums:
+            result^=num
+        return result
