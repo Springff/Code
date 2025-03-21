f6ecbdc1d3845be1a8059b3640b6964dd9f41d03
Chuny <166700944+Springff@users.noreply.github.com>
2025-03-21 15:45:10 +0800

    Create 移动零.py

---
+++ 双指针/移动零.py
@@ -0,0 +1,16 @@
+# 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+# 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        left = 0
+        for i in range(n):
+            if nums[i]!=0:
+                nums[left],nums[i]=nums[i],nums[left]
+                left+=1
